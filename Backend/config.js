5b9799c2e43cc3b588f80e0444b5a9da0101a6e6
omkarparkhande <77226884+omkarparkhande@users.noreply.github.com>
2024-01-09 16:36:36 +0530

    first commit

---
+++ Backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = 3000;
+
+
+export const mongoDBURL = "mongodb+srv://root:root@book-store-mern.fai9vts.mongodb.net/books-collection?retryWrites=true&w=majority"
---
+++ Backend/index.js
@@ -0,0 +1,39 @@
+import express, { response } from "express";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
+import booksRoute from "./routes/booksRoute.js"
+import cors from 'cors';
+
+const app = express();
+
+app.use(express.json())
+
+app.use(cors())
+
+// app.use(cors({
+//     origin: "http://localhost:3000/",
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     allowedHeaders: ['Content-Type']
+// }));
+
+
+app.get('/', (req, res) => {
+    // console.log("first")
+    return res.status(234).send("<html><body><h1>Hello Node</h1></body></html>")
+})
+
+app.use('/books', booksRoute)
+
+mongoose
+    .connect(mongoDBURL)
+    .then(() => {
+        console.log("Database connected")   
+        app.listen(PORT, () => {
+            console.log(`App is listening on: ${PORT}`);
+        }) 
+    }).catch((err) => {
+        console.log(`error ${err.message}`)
+    });
+
+
+
