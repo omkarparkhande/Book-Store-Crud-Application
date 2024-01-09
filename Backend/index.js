a7da94399915fa82f2afb19e8845105789cf00b4
omkar parkhande <77226884+omkarparkhande@users.noreply.github.com>
2024-01-09 16:49:58 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -8,13 +8,13 @@ const app = express();
 
 app.use(express.json())
 
-app.use(cors())
+// app.use(cors())
 
-// app.use(cors({
-//     origin: "http://localhost:3000/",
-//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//     allowedHeaders: ['Content-Type']
-// }));
+app.use(cors({
+    origin: "https://book-store-crud-application.vercel.app/",
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type']
+}));
 
 
 app.get('/', (req, res) => {
